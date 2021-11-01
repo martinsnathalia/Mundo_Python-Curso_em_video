eb9804408a436ec8aa76cfe18e5cfd0aacb8029c
Nathália Martins <89808695+martinsnathalia@users.noreply.github.com>
2021-11-01 20:39:11 -0300

    Delete desafio027.py

--- CursoEmVideo-Python3-Mundo1/desafio027.py
+++
@@ -1,7 +0,0 @@
-# Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
-
-nome = str(input('Digite seu nome completo: ')).strip()
-lista_nome = nome.split()
-tamanho_lista = len(lista_nome)
-print('O primeiro nome será: {}'.format(lista_nome[0]))
-print('O último nome será: {}'.format(lista_nome[tamanho_lista-1]))
